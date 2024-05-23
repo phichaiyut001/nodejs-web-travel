a8846def7cffadc3ff35c7f731f58a55e72ad100
Phichaiyut <pichayutkingpila@gmail.com>
2024-05-23 14:25:43 +0700

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,14 @@
-// vite.config.js
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react()],
+
   server: {
     proxy: {
-      "/api": {
-        target: "https://nodejs-web-travel-2.onrender.com/api",
+      "/api/": {
+        target: "https://nodejs-web-travel.onrender.com/api",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
