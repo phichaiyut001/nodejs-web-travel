b5e6a2d087e2183742d43cf18ab2d8e0637e1efa
Phichaiyut <pichayutkingpila@gmail.com>
2024-05-23 14:37:18 +0700

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     proxy: {
       "/api": {
         target: "https://nodejs-web-travel.onrender.com/api",
-        changeOrigin: false,
+        changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
