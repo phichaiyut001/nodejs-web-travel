1cdeb06b7dd4ec1aab2fe63e43aed6bc1dac2531
Phichaiyut <pichayutkingpila@gmail.com>
2024-05-23 14:06:15 +0700

    d
    d

--- vite.render.config.js
+++ vite.render.config.js
@@ -1,17 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const isDevelopment = process.env.NODE_ENV === "development";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
 
   server: {
-    proxy: {
-      "/api": {
-        target: "https://nodejs-web-travel.onrender.com/api",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
+    proxy: isDevelopment
+      ? {
+          "/api": {
+            target: "https://nodejs-web-travel.onrender.com/api",
+            changeOrigin: true,
+            rewrite: (path) => path.replace(/^\/api/, ""),
+          },
+        }
+      : {},
   },
 });
